edee0deb9216b0970be9c93910bee970c085c91a
zlz6664 <1787412633@qq.com>
2021-11-24 11:22:09 +0800

    输出hello world，使用if else语句

---
+++ learning_01.py
@@ -0,0 +1,14 @@
+def print_hi(word):
+    print(f'hello {word}!')
+
+
+if 1 == 2:
+    print_hi('world')
+else:
+    print_hi("python")
+
+
+if __name__ == '__main__':
+    print('you are right!')
+else:
+    print('you are wrong!')
